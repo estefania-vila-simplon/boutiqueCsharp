94463de4dee3ab4a0cce1379b8046f3c0f6a9c34
Estefania Lopez <estefania.vila.simplon@gmail.com>
2021-10-09 23:35:19 +0200

    + routing config, navbar et pie vue partielle

--- BoutiqueCsharp/obj/Debug/netcoreapp3.1/Razor/Views/_ViewStart.cshtml.g.cs
+++ BoutiqueCsharp/obj/Debug/netcoreapp3.1/Razor/Views/_ViewStart.cshtml.g.cs
@@ -19,8 +19,15 @@ using BoutiqueCsharp.ViewModels;
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 2 "C:\Users\gloriaestefania.lope\Desktop\Personnel\C#\BoutiqueCsharp\BoutiqueCsharp\Views\_ViewImports.cshtml"
+using BoutiqueCsharp.Models;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"7091c65830b0329e613be026ede8a57552863778", @"/Views/_ViewStart.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"a72fa192b3b39ed982d21942a0982693cb6fdf8e", @"/Views/_ViewImports.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"8ddfce2846750b934476ed0ba995763f144138b3", @"/Views/_ViewImports.cshtml")]
     public class Views__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998

63d6a5a2d53549efe8e56955a00eda46075b80e9
Estefania Lopez <estefania.vila.simplon@gmail.com>
2021-10-10 14:18:56 +0200

    + HomeController, HomeViewModel et Home avec carrousel

---
+++ BoutiqueCsharp/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using BoutiqueCsharp.Models;
+using BoutiqueCsharp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoutiqueCsharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek = _pieRepository.PiesOfTheWeek
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ BoutiqueCsharp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BoutiqueCsharp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoutiqueCsharp.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; set; }
+    }
+}

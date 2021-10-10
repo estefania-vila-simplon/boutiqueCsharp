672940435105d71981fd010fd1e276a7f686df0c
Estefania Lopez <estefania.vila.simplon@gmail.com>
2021-10-10 16:02:48 +0200

    Panier modifié

--- BoutiqueCsharp/Models/AppDbContext.cs
+++ BoutiqueCsharp/Models/AppDbContext.cs
@@ -15,6 +15,7 @@ namespace BoutiqueCsharp.Models
 
         public DbSet<Pie> Pies { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
